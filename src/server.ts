b3673de3738d97b55e6112e11e39bfe8b88a8339
werliton <werliton.carlos@ceuma.br>
2020-09-02 23:36:40 -0300

    Configurando prettier

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,34 @@
-import './utils/module-alias'
-import { Server } from '@overnightjs/core'
-import { json, Application } from 'express'
-import { ForeCastController } from './controllers/forecast'
+import './utils/module-alias';
+import { Server } from '@overnightjs/core';
+import { json, Application } from 'express';
+import { ForeCastController } from './controllers/forecast';
 
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
 
-export class SetupServer extends Server{
-    constructor(private port = 3000){
-        super()
-    }
+  /**
+   * init
+   */
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    /**
-     * init
-     */
-    public init():void {
-        this.setupExpress()
-        this.setupControllers()
-    }
+  /**
+   * getApp
+   */
+  public getApp(): Application {
+    return this.app;
+  }
 
-    /**
-     * getApp
-     */
-    public getApp(): Application {
-        return this.app
-    }
+  private setupExpress(): void {
+    this.app.use(json());
+  }
 
-    private setupExpress(): void {
-        this.app.use(json())
-    }
-
-    private setupControllers():void {
-        const foreCastController = new ForeCastController()
-        this.addControllers([foreCastController])
-    }
-
+  private setupControllers(): void {
+    const foreCastController = new ForeCastController();
+    this.addControllers([foreCastController]);
+  }
 }
